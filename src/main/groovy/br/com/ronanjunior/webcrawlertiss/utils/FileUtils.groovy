ffda227753299c51dc314a496c9e6cc466905805
Ronan Vieira do Carmo Juior <ronanvcjunior@gmail.com>
2023-10-15 01:59:32 -0300

    feature: escrita em arquivo .csv

--- src/main/groovy/br/com/ronanjunior/webcrawlertiss/utils/FileUtils.groovy
+++ src/main/groovy/br/com/ronanjunior/webcrawlertiss/utils/FileUtils.groovy
@@ -1,19 +1,38 @@
 package br.com.ronanjunior.webcrawlertiss.utils
 
 class FileUtils {
-    FileOutputStream getOutputStreamForFile(String filePath) {
+    FileOutputStream getOutputStreamForFile(String filePath, Boolean modoAnexacao = false) {
         try {
-            def file = new File(filePath)
-            def parentDirectory = file.parentFile
+            File file = new File(filePath)
+            File parentDirectory = file.parentFile
             if (!parentDirectory.exists()) {
                 if (!parentDirectory.mkdirs()) {
                     throw new IOException("Falha ao criar o diretório: ${parentDirectory}")
                 }
             }
-            return new FileOutputStream(file)
+            return new FileOutputStream(file, modoAnexacao)
         } catch (IOException e) {
             e.printStackTrace()
             return null
         }
     }
+
+    boolean escreverNoArquivoCsv(String filePath, List<String> linha, Boolean modoAnexacao = true) {
+        FileOutputStream outputStream = getOutputStreamForFile(filePath, modoAnexacao)
+        if (outputStream) {
+            PrintWriter writer = new PrintWriter(outputStream)
+
+            try {
+                String linhaCsv = linha.join(',')
+                writer.println(linhaCsv)
+                writer.close()
+                return true
+            } catch (IOException e) {
+                e.printStackTrace()
+            }
+        }
+        return false
+    }
+
+
 }
